0759f3db006b7b457529f9196314fe99c7fd24c5
yagya <yagyagoel87@gmail.com>
2024-01-16 21:52:34 +0530

    added basic frontend using react

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react"
+export function CreateTodo(){
+    //react-query
+    const[title,setTitle] = useState("");
+    const[description,setDescription]= useState("");
+return <div>
+    <input style = {{padding :10,margin:10}} type="text" placeholder="title" onChange ={function(e){
+        const value = e.target.value;
+        setTitle(e.target.value);
+    }} /><br />
+    <input style = {{padding :10,margin:10}}  type="text" placeholder="description" onChange ={function(e){
+        const value = e.target.value;
+        setDescription(e.target.value);
+    }} /><br />
+   {useEffect(()=>{
+   <button style = 
+   {{padding :10,margin:10}} onClick = {()=>{
+    fetch("http://localhost:3000/todo",{
+        method : "POST",
+        body :JSON.stringify({
+            title :title,
+            description :description,
+
+        }),
+        headers :{
+            "Content-type": "application/json"
+        }
+    }).then (async function(res){
+        const json  = await res.json();
+        alert(json.msg);
+    })
+   }}>
+    add a todo</button>},[])}
+</div>
+}
